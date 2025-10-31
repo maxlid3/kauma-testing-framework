6673cfb1d77aff9372e96b1fca44bd5b1ee5a2e3
maxlid3 <73897207+maxlid3@users.noreply.github.com>
2025-10-31 20:20:55 +0100

    Added proper CLI-handling and removed a test-file

--- run_tests.py
+++ run_tests.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 from pathlib import Path
 
 from utils import run_docker
@@ -17,7 +18,19 @@ def gen_testcase_list():
     return testcase_list
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='kauma-testing-framework')
+    parser.add_argument('kauma_path', help='Path of your kauma file')
+    # parser.add_argument('-d', '--debug', action='store_true', help='Activate extended debug mode')
+    
+    try:
+        args = parser.parse_args()
+    except SystemExit:
+        print()
+        parser.print_help()
+        sys.exit(1)
+
+
     testcase_list = gen_testcase_list()
-    kauma_path = Path(sys.argv[1])
+    kauma_path = Path(args.kauma_path)
 
     run_docker(kauma_path, testcase_list)
